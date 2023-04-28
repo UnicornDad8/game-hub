f1951357906d156700179041b95779949826f07e
UnicornDad8 <unicorndad8@gmail.com>
2023-04-28 11:37:36 -0400

    Added switch and darkMode

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,49 @@
+import { ChangeEvent, useState } from "react";
 import logo from "../../assets/logo.webp";
+import Sun from "../../assets/Sun.svg";
+import Moon from "../../assets/Moon.svg";
 import style from "./Navbar.module.css";
 
 const Navbar = () => {
+  const [checked, updateChecked] = useState(false);
+
+  const setDarkMode = () => {
+    document.querySelector("body")?.setAttribute("data-theme", "dark");
+  };
+
+  const setLightMode = () => {
+    document.querySelector("body")?.setAttribute("data-theme", "ligth");
+  };
+
+  const toggleTheme = (e: ChangeEvent) => {
+    updateChecked(!checked);
+
+    if (!checked) {
+      setDarkMode();
+    } else {
+      setLightMode();
+    }
+  };
+
   return (
     <nav className={style["navbar"]}>
-      <img src={logo} alt="logo" width="60px" height="60px" />
-      Navbar
+      <div className={style["navbar-logo-text"]}>
+        <img src={logo} alt="logo" width="60px" height="60px" />
+        <h3>Navbar</h3>
+      </div>
+      <div>
+        <label
+          className={`${style["Toggle"]} ${
+            checked ? style["Toggle--checked"] : style[""]
+          }`}
+        >
+          <input type="checkbox" value={checked} onChange={toggleTheme} />
+
+          <div className={style["Toggle__slider"]} />
+          <small className={style["Toggle__text"]}>Night</small>
+          <small className={style["Toggle__text"]}>Day</small>
+        </label>
+      </div>
     </nav>
   );
 };
