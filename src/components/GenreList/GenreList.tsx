2099c9fc32b1c5b2d21c86a804f5386bae6bacb8
UnicornDad8 <unicorndad8@gmail.com>
2023-05-08 12:04:02 -0400

    Clean up the genres

--- src/components/GenreList/GenreList.tsx
+++ src/components/GenreList/GenreList.tsx
@@ -14,33 +14,36 @@ const GenreList = ({ selectedGenre, onSelectGenre }: GenreListProps) => {
   if (error) return null;
 
   return (
-    <ul className={style["genre-container"]}>
-      {isLoading && (
-        <div className="center-spinner">
-          <div className="spinner-border" />
-        </div>
-      )}
-      {data.map((genre) => (
-        <li
-          key={genre.id}
-          className={
-            genre.id === selectedGenre?.id
-              ? `${style["genre-item"]} ${style["highlight"]}`
-              : style["genre-item"]
-          }
-        >
-          <Button color="link" onClick={() => onSelectGenre(genre)}>
-            <img
-              src={getCroppedImageUrl(genre.image_background)}
-              alt={`${genre.name} genre`}
-              width={60}
-              height={60}
-            />
-            <p>{genre.name}</p>
-          </Button>
-        </li>
-      ))}
-    </ul>
+    <div className={style["genre-container"]}>
+      <h2>Genres</h2>
+      <ul>
+        {isLoading && (
+          <div className="center-spinner">
+            <div className="spinner-border" />
+          </div>
+        )}
+        {data.map((genre) => (
+          <li
+            key={genre.id}
+            className={
+              genre.id === selectedGenre?.id
+                ? `${style["genre-item"]} ${style["highlight"]}`
+                : style["genre-item"]
+            }
+          >
+            <Button color="link" onClick={() => onSelectGenre(genre)}>
+              <img
+                src={getCroppedImageUrl(genre.image_background)}
+                alt={`${genre.name} genre`}
+                width={60}
+                height={60}
+              />
+              <p>{genre.name}</p>
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
