f9d9b209e91c9ed8d605a32aef6f3e7c372ad1e5
UnicornDad8 <unicorndad8@gmail.com>
2023-05-23 14:27:36 -0400

    Build game screenshots

---
+++ src/components/GameScreenshots/GameScreenshots.tsx
@@ -0,0 +1,26 @@
+import useScreenshots from "../../hooks/useScreenshots";
+import style from "./GameScreenshots.module.css";
+
+interface GameScreenshotsProps {
+  gameId: number;
+}
+
+const GameScreenshots = ({ gameId }: GameScreenshotsProps) => {
+  const { data, isLoading, error } = useScreenshots(gameId);
+
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  return (
+    <div className={style["screenshots-grid"]}>
+      {data?.results.map((file) => (
+        <div key={file.id} className={style["sreenshot-box"]}>
+          <img src={file.image} width={700} height={400} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default GameScreenshots;
